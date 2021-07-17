36039b4a30f4a562da1eb9b67d9c4b1450fa40fa
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-07-17 20:47:50 +0200

    Add files via upload

---
+++ mathe_question_generator.py
@@ -0,0 +1,61 @@
+# \n
+
+print("""
+
+This is a simple math equation generator. 
+This will generate a set of math equations
+
+""")
+
+import random
+
+def print_plus_equation():
+    print(str(random.randint(0,10000)) + "\n____ +\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ +\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ +\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ +\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n\n\n\n")
+
+print_plus_equation()
+
+
+def print_minus_equation():
+    print(str(random.randint(0,10000)) + "\n____ -\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ -\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ -\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ -\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n\n\n\n")
+
+print_minus_equation()
+
+
+
+def print_multiply_equation():
+    print(str(random.randint(0,10000)) + "\n____ x\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ x\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ x\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ x\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n\n\n\n")
+
+print_multiply_equation()
+
+def print_devide_equation():
+    print(str(random.randint(0,10000)) + "\n____ %\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ %\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ %\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n")
+    print(str(random.randint(0,10000)) + "\n____ %\n" +  str(random.randint(0,10000)) + "\n====")
+    print("\n\n\n\n")
+
+print_devide_equation()
